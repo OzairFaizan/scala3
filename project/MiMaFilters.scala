1e524e512141f2a61c3af7924def23c22f784d22
Ozair Faizan <ahmad.faizan@epfl.ch>
2026-03-23 17:58:51 +0100

    Add `assumeTerminates()` allowing user to skip termination checking on a block

--- project/MiMaFilters.scala
+++ project/MiMaFilters.scala
@@ -18,6 +18,8 @@ object MiMaFilters {
         ProblemFilters.exclude[MissingClassProblem]("scala.language$experimental$safe$"),
         ProblemFilters.exclude[MissingClassProblem]("scala.annotation.terminates"),
         ProblemFilters.exclude[MissingClassProblem]("scala.annotation.decreasesBy"),
+        ProblemFilters.exclude[MissingClassProblem]("scala.util.assumeTerminates"),
+        ProblemFilters.exclude[MissingClassProblem]("scala.util.assumeTerminates$"),
     ))
 
     val BackwardsBreakingChanges: Map[String, Seq[ProblemFilter]] = Map(
